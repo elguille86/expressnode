ce088d6b8887c97ddf43840a336f015902d3f477
unknown <g.rodriguez.p@hotmail.com>
2015-02-27 11:43:37 -0500

    instalano body-parser, para usar datos de formulario

--- servidor01.js
+++ servidor01.js
@@ -1,5 +1,11 @@
 // Usando el Framework Express
 var pais = 'Peru';
+var basededatos ={
+	usuario : "guillermo",
+	clave : "123456"
+}
+
+
 console.log ('Servidor Iniciado');
  
 // Invocamos el modulo express
@@ -19,12 +25,13 @@ web.get('/' , function(req , res){
 
 // Nosta si ingresados colocando 127.0.0.1:6969/entrar nos mostrar un error por que solo se ingresa con el metodo POST
 web.post('/entrar' , function(req , res){
-	res.send('has ingresado con el metodo POST' );
+	console.log (req);
+	//res.send('has ingresado con el metodo POST' );
 });
 
 web.get('/test' , function(req , res){
-	// Nota las Varaibles aparecen el requiere
-	// En navegador :  /test?auto=kia&color=azul');
+	// Nota las Varaibles aparecen el requiere -> uso de varialbles GET
+	// En navegador :  /test?auto=kia&color=azul') ;
 	// Con este puerdo lear variables del URL 
 	res.send('Tu Auto es '+ req.query.auto + ' y tu Color es '+ req.query.color);
 	console.log (req);
